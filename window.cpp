8f4f1b7f0970916838fba82112af2a43a8aa2818
Nádudvari Ákos <nadudvari.akos@hallgato.itk.ppke.hu>
2022-04-11 15:12:12 +0200

    Add flashing bar to InputTextBox

--- window.cpp
+++ window.cpp
@@ -12,24 +12,21 @@ int Window::event_loop() {
     int focus = -1;
     gin.timer(500);
     while(gin >> ev ) {
-        if(ev.type == ev_timer && focus != -1) {
-            _widgets[focus]->handle(ev);
-            draw();
-            continue;
-        }
-
         int old_focus = focus;
-        if (ev.type == ev_mouse && ev.button == btn_left)
-            for (size_t i = 0; i < _widgets.size(); i++)
-                if (_widgets[i]->is_selected(ev))
+        if (ev.type == ev_mouse && ev.button == btn_left) {
+            focus = -1;
+            for (size_t i = 0; i < _widgets.size(); i++) {
+                if (_widgets[i]->is_selected(ev)) {
                         focus = i;
-
-        if (focus != -1) {
-            if(old_focus != focus && old_focus != -1)
-                _widgets[old_focus]->unfocus();
-            _widgets[focus]->handle(ev);
-            old_focus = focus;
+                }
+            }
         }
+
+        if(old_focus != focus && old_focus != -1)
+            _widgets[old_focus]->unfocus();
+        if(focus != -1)
+            _widgets[focus]->handle(ev);
+
         event_handler(ev);
         draw();
     }
